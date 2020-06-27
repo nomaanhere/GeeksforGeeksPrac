d778d7185b28e2e0a08fbddf8e852f97dd39bc3e
Nomaan Parmar <mohamadnomaan.p@somaiya.edu>
2020-06-27 20:30:30 +0530

    Create medianArray.cpp

---
+++ Practice_Array/medianArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	//code
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a,a+n);
+        if(n%2 ==0){
+            cout<<(a[n/2]+a[n/2-1])/2<<endl;
+        }else {
+            cout<<a[n/2]<<endl;
+        }
+    }
+	return 0;
+}
